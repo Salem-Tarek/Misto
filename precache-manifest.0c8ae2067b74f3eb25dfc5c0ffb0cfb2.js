248f5925520d4cf37ac3ca1b4514f6fff5b16b54
Salem-Tarek <salemtarek224@gmail.com>
2022-10-16 21:31:36 +0200

    Deploying Dist Folder

---
+++ precache-manifest.0c8ae2067b74f3eb25dfc5c0ffb0cfb2.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1beed59e2fd5c90320",
+    "url": "/Misto/css/app.818df92f.css"
+  },
+  {
+    "revision": "a1151318b2c24e985a48",
+    "url": "/Misto/css/chunk-vendors.ef40ad22.css"
+  },
+  {
+    "revision": "530a4134ffa5ce5817a0ea5e08815599",
+    "url": "/Misto/img/about.530a4134.jpg"
+  },
+  {
+    "revision": "0ba4a6d0d8e4ae0300a96f8526826a4c",
+    "url": "/Misto/img/blog1.0ba4a6d0.jpg"
+  },
+  {
+    "revision": "5c3baff8249b7fe64c17bbea1672ee5c",
+    "url": "/Misto/img/electronics.5c3baff8.jpg"
+  },
+  {
+    "revision": "745e6e3d8e7d1f06767b2fca91eea59f",
+    "url": "/Misto/img/jewelery.745e6e3d.jpg"
+  },
+  {
+    "revision": "1f1be0fcd7452a12291f8ff1fe707c8b",
+    "url": "/Misto/img/man.1f1be0fc.jpg"
+  },
+  {
+    "revision": "034d3ec5ee9d8da6222236008160320a",
+    "url": "/Misto/img/newCollection.034d3ec5.jpg"
+  },
+  {
+    "revision": "a5aab770d9b04eb42656186a1dd23792",
+    "url": "/Misto/img/offer50.a5aab770.jpg"
+  },
+  {
+    "revision": "151746d93d7286b2b79ee843e71af9c5",
+    "url": "/Misto/img/woman.151746d9.jpg"
+  },
+  {
+    "revision": "7e560cf13826f12f878ab87bc52063b8",
+    "url": "/Misto/index.html"
+  },
+  {
+    "revision": "3b1beed59e2fd5c90320",
+    "url": "/Misto/js/app.1babc1cc.js"
+  },
+  {
+    "revision": "a1151318b2c24e985a48",
+    "url": "/Misto/js/chunk-vendors.02ef085f.js"
+  },
+  {
+    "revision": "f05454569fb92a062787fdb0f0e3b73d",
+    "url": "/Misto/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Misto/robots.txt"
+  }
+]);
